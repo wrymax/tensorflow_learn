b18a8c0a93c80bd05ddaf5f0c2f274d2178924d2
王若愚 <wrymax@qq.com>
2017-09-25 23:02:01 +0800

    try estimator and iris training

--- CNN/MNIST_CNN.py
+++ CNN/MNIST_CNN.py
@@ -4,6 +4,8 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
@@ -81,14 +83,16 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
+  writer = tf.summary.FileWriter("/tmp/mnist_demo/1")
+  writer.add_graph(sess.graph)
   sess.run(tf.global_variables_initializer())
-  
+
   for i in range(20000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
       train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
       print('step %d, training accuracy %g' % (i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    
+
   # Test accuracy
   print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
